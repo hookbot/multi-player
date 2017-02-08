fb0cccc5cf3807bdaad2d3b46377591ac6d6ddf0
Travis Chase <gaudeon@cpan.org>
2017-02-08 11:38:19 -0700

    created asset manager that supports loading and initializing tilemap atlas sound and image
    started on flag sprites
    
    moved forest tilemap code into asset manager

---
+++ src/objects/asset_manager.js
@@ -0,0 +1,94 @@
+// namespace
+var App = App || {};
+
+App.AssetManager = (function () {
+    "use strict";
+
+    var fn = function (game) {
+        this.game = game;
+
+        this.config = this.config || {};
+        this.config.assets = this.config.assets || this.game.cache.getJSON('assetsConfig');
+
+        // init tilesets
+        this.tilesets = {};
+    };
+
+    fn.prototype.loadAssets = function () { this._process('load'); };
+
+    fn.prototype.initAssets = function () { this._process('init'); };
+
+    // run a load or init...
+    fn.prototype._process = function (action) {
+        if (!action.match(/^(load|init)$/)) return;
+
+        _.each(_.keys(this.config.assets), (function (key) {
+            var asset_data = this.config.assets[key];
+            var asset_type = asset_data.type;
+            if (asset_type && "function" === typeof this[action + '_' + asset_type]) {
+                this[action + '_' + asset_type](key, asset_data);
+            }
+            else {
+                console.log('Asset handler of type "' + asset_type + '" not found for key "' + key + '" while trying to "' + action + '"');
+            }
+        }).bind(this));
+    };
+
+    // atlas
+    fn.prototype.load_atlas = function (key, data) {
+        this.game.load.atlas(key, data.file, data.json, Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
+    };
+
+    fn.prototype.init_atlas = function (key, data) {
+        // nothing to do yet
+    };
+
+    // tilemap
+    fn.prototype.load_tilemap = function (key, data) {
+        this.game.load.image(data.tileset_image.key, data.tileset_image.file);
+        this.game.load.tilemap(key, data.json, null, Phaser.Tilemap.TILED_JSON);
+    };
+
+    fn.prototype.init_tilemap = function (key, data) {
+        this.tilesets[key] = {};
+
+        this.tilesets[key].map = this.game.add.tilemap(key);
+        this.tilesets[key].map.addTilesetImage(data.tileset_image.tiled_set_name, data.tileset_image.key);
+
+        this.tilesets[key].layers = {};
+
+        _.each(_.keys(data.layers), (function (layer) {
+            var tiled_layer_name = data.layers[layer].tiled_layer_name;
+
+            this.tilesets[key].layers[layer] = this.tilesets[key].map.createLayer(tiled_layer_name);
+
+            var collision_id_first = data.layers[layer].collision_id_first;
+            var collision_id_last  = data.layers[layer].collision_id_last;
+            if (collision_id_first && collision_id_last) {
+                this.tilesets[key].map.setCollisionBetween(collision_id_first, collision_id_last, true, tiled_layer_name);
+            }
+        }).bind(this));
+    };
+
+    fn.prototype.get_tilemap = function (key) { return this.tilesets[key]; };
+
+    // image
+    fn.prototype.load_image = function (key, data) {
+        this.game.load.image(data.key, data.file);
+    };
+
+    fn.prototype.init_image = function (key, data) {
+        // nothing to do yet
+    };
+
+    // sound
+    fn.prototype.load_sound = function (key, data) {
+        this.game.load.sound(data.key, data.file);
+    };
+
+    fn.prototype.init_sound = function (key, data) {
+        // nothing to do yet
+    };
+
+    return fn;
+})();
---
+++ src/objects/flags/flag_orange_left.js
@@ -0,0 +1,15 @@
+// namespace
+var App = App || {};
+
+App.FlagOrangeLeft = (function () {
+    "use strict";
+
+    var fn = function (game, x, y) {
+        App.Flag.call(this, game, x, y, key, frame);
+    };
+
+    fn.prototype = Object.create(App.Flag.prototype);
+    fn.prototype.constructor = fn;
+
+    return fn;
+})();
